369a2f22d58d0ec1124c7b17d8fc52596883672f
Suvash Joshi <suvash2077@gmail.com>
2024-01-12 22:33:37 +0545

    Fixed command generation

--- src/app/components/Output.tsx
+++ src/app/components/Output.tsx
@@ -4,28 +4,66 @@ import Neofetch from "./commands/Neofetch";
 import Projects from "./commands/Projects";
 
 export default function Output({ command }: { command: string }) {
-  switch (command) {
-    case "clear":
-      return <></>;
+  return (
+    <>
+      <p className="mr-auto min-w-fit text-green-500">
+        sushi@linux-desktop:~$<span className="ml-2 text-white">{command}</span>
+      </p>
+      {command && (
+        <div>
+          {(() => {
+            switch (command) {
+              // case "clear":
+              //   return null;
 
-    case "help":
-      return <Help />;
+              case "help":
+                return <Help />;
 
-    case "about":
-      return <About />;
+              case "about":
+                return <About />;
 
-    case "projects":
-      return <Projects />;
+              case "projects":
+                return <Projects />;
 
-    case "neofetch":
-      return <Neofetch />;
+              case "neofetch":
+                return <Neofetch />;
 
-    case "repo":
-      return <p>Redirecting you to repo....</p>;
+              case "repo":
+                return <p>Redirecting you to repo....</p>;
 
-    default:
-      return (
-        <p>{`Command '${command}' not found, type 'help' to see all commands.`}</p>
-      );
-  }
+              default:
+                return (
+                  <p>{`Command '${command}' not found, type 'help' to see all commands.`}</p>
+                );
+            }
+          })()}
+        </div>
+      )}
+    </>
+  );
+
+  // switch (command) {
+  //   case "clear":
+  //     return <></>;
+
+  //   case "help":
+  //     return <Help />;
+
+  //   case "about":
+  //     return <About />;
+
+  //   case "projects":
+  //     return <Projects />;
+
+  //   case "neofetch":
+  //     return <Neofetch />;
+
+  //   case "repo":
+  //     return <p>Redirecting you to repo....</p>;
+
+  //   default:
+  //     return (
+  //       <p>{`Command '${command}' not found, type 'help' to see all commands.`}</p>
+  //     );
+  // }
 }
--- src/app/components/Terminal.tsx
+++ src/app/components/Terminal.tsx
@@ -11,12 +11,17 @@ export default function Terminal() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (command === "clear") {
+      setCommandHistory([]);
+      console.log("clear command");
+    } else {
+      setCommandHistory([...commandHistory, command]);
+    }
     setNewCommand(command);
-    setCommandHistory([...commandHistory, command]);
     setCommand("");
   };
   return (
-    <div className="flex h-1/2 w-full flex-col bg-[#2C001E]/90 p-1 text-white">
+    <div className="flex h-1/2 w-full flex-col-reverse overflow-auto bg-[#2C001E]/90 p-1 text-white">
       <form onSubmit={(e) => handleSubmit(e)} className="flex flex-row">
         <p className="mr-auto min-w-fit text-green-500">
           sushi@linux-desktop<span>:~$</span>
@@ -28,8 +33,10 @@ export default function Terminal() {
           className="ml-2 w-full bg-inherit outline-none"
         />
       </form>
-      <section className="flex-1 overflow-auto">
-        {newCommand && <Output command={newCommand} />}
+      <section className="flex-1">
+        {commandHistory.map((cmd) => (
+          <Output key={cmd} command={cmd} />
+        ))}
       </section>
     </div>
   );
